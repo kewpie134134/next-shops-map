51031828918cf2fd14e8a3a258a245bde8a19cab
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-06-13 15:53:54 +0900

    [add] @react-google-map/api を使用して地図を表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,52 @@
 import styles from 'styles/Home.module.css';
+import { GoogleMap, LoadScript, InfoWindow } from '@react-google-maps/api';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
+
+const containerStyle = {
+  width: '100%',
+  height: '100vh'
+};
+
+const center = {
+  lat: 35.69575,
+  lng: 139.77521
+};
+
+const positionAkiba = {
+  lat: 35.69731,
+  lng: 139.7747
+};
+
+const positionIwamotocho = {
+  lat: 35.69397,
+  lng: 139.7762
+};
+
+const divStyle = {
+  background: 'white',
+  fontSize: 7.5
+};
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <LoadScript googleMapsApiKey="AIzaSyB9ozf8pDBbJW9lBXlTkaMrT4c7YVv-lhw">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
+          <InfoWindow position={positionAkiba}>
+            <div style={divStyle}>
+              <h1>秋葉原オフィス</h1>
+            </div>
+          </InfoWindow>
+          <InfoWindow position={positionIwamotocho}>
+            <div style={divStyle}>
+              <h1>岩本町オフィス</h1>
+            </div>
+          </InfoWindow>
+        </GoogleMap>
+      </LoadScript>
       <Footer />
     </div>
   );
