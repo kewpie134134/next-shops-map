e92467149c9fcb3e50c441607728960fd32f8c5e
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-06-13 17:07:11 +0900

    [update] コンポーネント化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,14 @@
 import styles from 'styles/Home.module.css';
-import { GoogleMap, LoadScript, InfoWindow } from '@react-google-maps/api';
 
 import Header from 'components/Header';
+import Map from 'components/Map';
 import Footer from 'components/Footer';
-
-const containerStyle = {
-  width: '100%',
-  height: '100vh'
-};
-
-const center = {
-  lat: 35.69575,
-  lng: 139.77521
-};
-
-const positionAkiba = {
-  lat: 35.69731,
-  lng: 139.7747
-};
-
-const positionIwamotocho = {
-  lat: 35.69397,
-  lng: 139.7762
-};
-
-const divStyle = {
-  background: 'white',
-  fontSize: 7.5
-};
 
 const Home = () => {
   return (
     <div className={styles.container}>
       <Header />
-      <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-      >
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-          <InfoWindow position={positionAkiba}>
-            <div style={divStyle}>
-              <h1>秋葉原オフィス</h1>
-            </div>
-          </InfoWindow>
-          <InfoWindow position={positionIwamotocho}>
-            <div style={divStyle}>
-              <h1>岩本町オフィス</h1>
-            </div>
-          </InfoWindow>
-        </GoogleMap>
-      </LoadScript>
+      <Map />
       <Footer />
     </div>
   );
